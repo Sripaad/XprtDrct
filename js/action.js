229b658f587f9ad784874cec1029b2f089252b46
Sripaad srinivasan <sripaad751@gmail.com>
2021-02-06 19:18:24 +0530

    overall change
    Shift from exports to call center

--- js/action.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 10) {
-    $('.exim-navbar').addClass('full-nav');
-  } else {
-    $('.exim-navbar').removeClass('full-nav');
-  }
-});
